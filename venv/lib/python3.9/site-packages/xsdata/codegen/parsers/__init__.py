825fd630ec0683bc4dae1491a927b8b683c3b59d
Simon Schubotz <simon@phoebe.bcs.e-technik.tu-darmstadt.de>
2026-01-30 13:44:01 +0100

    Remove venv from repository and add to gitignore

--- venv/lib/python3.9/site-packages/xsdata/codegen/parsers/__init__.py
+++
@@ -1,9 +0,0 @@
-from xsdata.codegen.parsers.definitions import DefinitionsParser
-from xsdata.codegen.parsers.dtd import DtdParser
-from xsdata.codegen.parsers.schema import SchemaParser
-
-__all__ = [
-    "DefinitionsParser",
-    "DtdParser",
-    "SchemaParser",
-]
